506a93c674427275f053ee2782cb2050fabff2fa
Ryan Hefner <hi@ryanhefner.com>
2021-12-06 22:09:18 -0500

    Update Babel presets

--- tools/babel-preset.js
+++ tools/babel-preset.js
@@ -1,16 +1,16 @@
-const BABEL_ENV = process.env.BABEL_ENV;
-const building = BABEL_ENV != undefined && BABEL_ENV !== 'cjs';
+const BABEL_ENV = process.env.BABEL_ENV
+const building = BABEL_ENV != undefined && BABEL_ENV !== 'cjs'
 
 const plugins = [
   '@babel/plugin-proposal-class-properties',
   '@babel/plugin-proposal-object-rest-spread',
   '@babel/plugin-transform-runtime',
-];
+]
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(
     'babel-plugin-dev-expression',
-  );
+  )
 }
 
 module.exports = () => {
@@ -19,15 +19,16 @@ module.exports = () => {
       test: {
         presets: [
           '@babel/preset-env',
+          '@babel/preset-react',
         ],
       },
     },
     presets: [
       ['@babel/preset-env', {
-        'loose': true,
         'modules': building ? false : 'commonjs',
       }],
+      '@babel/preset-react',
     ],
     plugins: plugins,
-  };
-};
+  }
+}
