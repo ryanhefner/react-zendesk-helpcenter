e2140fa3e4d62a4ebf34ee9ad43ab49b422659a1
Ryan Hefner <hi@ryanhefner.com>
2022-01-25 10:53:00 -0500

    Update fetcher to include fetch options

--- src/libs/fetch.js
+++ src/libs/fetch.js
@@ -1,5 +1,17 @@
 // @todo Add error handling - Ryan
 export default async function fetcher(...args) {
-  const res = await fetch(...args)
+  const [url, options] = args
+
+  const fetchOptions = {
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    mode: 'no-cors',
+    ...options,
+  }
+
+  const res = await fetch(url, fetchOptions)
+
   return res.json()
 }
